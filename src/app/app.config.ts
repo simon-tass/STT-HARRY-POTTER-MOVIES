76202c47e824b5cb61bed17b843d546f832e79b2
simon-tass <simon.tassaert@soprasteria.com>
2024-12-04 17:20:02 +0100

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,6 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(),
   ],
 };
